0470b356c7b913f6f5e148301608c81b15a906ea
ifeola <nomorelimit11@gmail.com>
2025-01-05 12:14:13 +0100

    Added new functionality. Add new To-do form functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+const ongoingTodos = document.querySelectorAll(".ongoing__todos .todo");
+const completedTodos = document.querySelectorAll(".completed__todos .todo");
+const completed = document.querySelector(".completed__todos");
+const ongoing = document.querySelector(".ongoing__todos");
+const todoInput = document.querySelector("#todo__input");
+const todoForm = document.querySelector("#todo__form");
